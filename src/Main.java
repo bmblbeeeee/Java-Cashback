93360dc2219c7518e2d4828a3ece98996426f3dc
Anastasia Valova <morugina.anast@mail.ru>
2024-03-24 15:04:30 +0300

    Added everything

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 1200;
+        int z = 100;
+        if ( x > 1000) {
+            int y = x/z;
+            System.out.println(y);
+        }
+        else {
+            System.out.println("Нет бонусов");
+        }
+    }
+}
