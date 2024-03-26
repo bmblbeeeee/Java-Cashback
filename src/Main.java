2fe63dfc84295b6c7e2f5374b41559d0d0e2ecc5
Anastasia Valova <morugina.anast@mail.ru>
2024-03-26 22:39:13 +0300

    Improving

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 1200;
-        int z = 100;
-        if ( x > 1000) {
-            int y = x/z;
-            System.out.println(y);
-        }
-        else {
+        int TopUpAmount = 1200;
+        int interval = 100;
+        if (TopUpAmount > 1000) {
+            int Bonus = TopUpAmount / interval;
+            System.out.println("Итоговый счет:");
+            System.out.println(TopUpAmount + Bonus);
+            System.out.println("Бонусов начислено:");
+            System.out.println(Bonus);
+        } else {
             System.out.println("Нет бонусов");
         }
     }
