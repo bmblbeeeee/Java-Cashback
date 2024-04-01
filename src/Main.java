a0275ffd3ec6409ffd897957b255d56dafa15f4c
Anastasia Valova <morugina.anast@mail.ru>
2024-04-01 22:31:03 +0300

    Small letters

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int TopUpAmount = 1200;
+        int topUpAmount = 1200;
         int interval = 100;
-        if (TopUpAmount > 1000) {
-            int Bonus = TopUpAmount / interval;
+        if (topUpAmount > 1000) {
+            int bonus = topUpAmount / interval;
             System.out.println("Итоговый счет:");
-            System.out.println(TopUpAmount + Bonus);
+            System.out.println(topUpAmount + bonus);
             System.out.println("Бонусов начислено:");
-            System.out.println(Bonus);
+            System.out.println(bonus);
         } else {
             System.out.println("Нет бонусов");
         }
